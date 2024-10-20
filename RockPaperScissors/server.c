c5d8cbc3029c25039cef4c709ec470689b9a3d44
Ludensburger <mendozaryu08@gmail.com>
2024-10-21 02:32:19 +0800

    Refactor server.c to add case-insensitive comparison for determining the winner in Rock, Paper, Scissors game

--- RockPaperScissors/server.c
+++ RockPaperScissors/server.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,17 +11,31 @@
 
 const char *choices[] = {"Rock", "Paper", "Scissors"};
 
+void to_lowercase(char *str) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = tolower(str[i]);
+    }
+}
+
 const char *determine_winner(const char *client_choice, char *server_choice) {
     int random_choice = rand() % 3;                // Random choice for the server
     strcpy(server_choice, choices[random_choice]); // Copy server choice to output
     printf("Server choice: %s\n", server_choice);
 
+    // Convert choices to lowercase for comparison
+    char client_choice_lower[BUFFER_SIZE];
+    char server_choice_lower[BUFFER_SIZE];
+    strcpy(client_choice_lower, client_choice);
+    strcpy(server_choice_lower, server_choice);
+    to_lowercase(client_choice_lower);
+    to_lowercase(server_choice_lower);
+
     // Determine the winner
-    if (strcmp(client_choice, server_choice) == 0) {
+    if (strcmp(client_choice_lower, server_choice_lower) == 0) {
         return "It's a draw!";
-    } else if ((strcmp(client_choice, "Rock") == 0 && strcmp(server_choice, "Scissors") == 0) ||
-               (strcmp(client_choice, "Paper") == 0 && strcmp(server_choice, "Rock") == 0) ||
-               (strcmp(client_choice, "Scissors") == 0 && strcmp(server_choice, "Paper") == 0)) {
+    } else if ((strcmp(client_choice_lower, "rock") == 0 && strcmp(server_choice_lower, "scissors") == 0) ||
+               (strcmp(client_choice_lower, "paper") == 0 && strcmp(server_choice_lower, "rock") == 0) ||
+               (strcmp(client_choice_lower, "scissors") == 0 && strcmp(server_choice_lower, "paper") == 0)) {
         return "You win!";
     } else {
         return "You lose!";
@@ -102,7 +117,7 @@ int main() {
 
             // Format response to include the server's choice
             char response[BUFFER_SIZE];
-            snprintf(response, sizeof(response), "%s (Computer picked: %s)", result, server_choice);
+            snprintf(response, sizeof(response), "%s (Computer picked: %s)\n", result, server_choice);
 
             send(clientSocket, response, strlen(response), 0); // Send result to client
         } else if (bytesRead == 0) {
