c2d8cee0d4967b8a1e304c2f8c26a8c1be5ab0b6
Ludensburger <mendozaryu08@gmail.com>
2024-10-29 19:18:23 +0800

    Implement player functionality for displaying cards, calculating score, and resetting player states

---
+++ BlackJack/StackModular/player.h
@@ -0,0 +1,22 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <winsock2.h>
+
+#define MAX_PLAYERS 4
+
+typedef struct {
+    SOCKET socket;
+    char name[50];
+    int hand[10];
+    int hand_size;
+    int score;
+    int is_active;
+    char color[10];
+} Player;
+
+void display_player_cards(Player *player);
+void calculate_score(Player *player);
+void reset_player_states(Player players[], int player_count);
+
+#endif // PLAYER_H
