a4d06a4567d1fc4d1efa6b0610f27fc014a5259d
Ludensburger <mendozaryu08@gmail.com>
2024-10-29 19:18:50 +0800

    Implement stack functionality for managing cards in the game

---
+++ BlackJack/StackModular/stack.h
@@ -0,0 +1,22 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define STACK_SIZE 52
+
+typedef struct {
+    int cards[STACK_SIZE];
+    int top;
+} Stack;
+
+void initializeStack(Stack *stack);
+int isEmpty(Stack *stack);
+int isFull(Stack *stack);
+void push(Stack *stack, int card);
+int pop(Stack *stack);
+void shuffleCards(int *cards, int size);
+void fillStack(Stack *stack);
+void resetAndFillStack(Stack *stack);
+void cleanStack(Stack *stack);
+void printStack(Stack *stack);
+
+#endif // STACK_H
