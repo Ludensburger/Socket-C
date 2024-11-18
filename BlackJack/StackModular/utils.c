cc591be070d7eaeee8454507fdd0b12d41061231
Ludensburger <mendozaryu08@gmail.com>
2024-11-19 01:21:41 +0800

    Add function to retrieve color codes for console output

--- BlackJack/StackModular/utils.c
+++ BlackJack/StackModular/utils.c
@@ -20,6 +20,19 @@ const char *card_to_string(int card) {
     return buffer;
 }
 
+const char *getColor(int choice) {
+    const char *colors[] = {
+        "\033[31m", // Red
+        "\033[32m", // Green
+        "\033[33m", // Yellow
+        "\033[34m", // Blue
+        "\033[35m", // Magenta
+        "\033[36m", // Cyan
+    };
+
+    return colors[choice];
+}
+
 const char *getRandomColor() {
     const char *colors[] = {
         "\033[31m", // Red
