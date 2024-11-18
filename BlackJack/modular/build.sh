166f03f4fca824323ad58274fe59625b5e1087c5
Ludensburger <mendozaryu08@gmail.com>
2024-11-19 02:08:08 +0800

    Refactor build scripts for BlackJack game: consolidate compilation and linking steps

--- BlackJack/modular/build.sh
+++ BlackJack/modular/build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-set -e  # Exit immediately if a command exits with a non-zero status
-set -x  # Print each command before it is executed
-
 # Compile each source file into an object file
+gcc -c player.c -o player.o
+gcc -c stack.c -o stack.o
 gcc -c utils.c -o utils.o
-gcc -c client.c -o client.o
+gcc -c game_state.c -o game_state.o
 gcc -c server.c -o server.o
+gcc -c client.c -o client.o
 
-# Link object files to create the server executable
-gcc server.o utils.o -o server.exe -lws2_32
+# Link all object files to create the server executable
+gcc player.o stack.o utils.o game_state.o server.o -o server.exe -lws2_32
 
-# Link object files to create the client executable
-gcc client.o utils.o -o client.exe -lws2_32
-
-echo "Build complete. Executables are server.exe and client.exe."
+# Link all object files to create the client executable
+gcc player.o stack.o utils.o game_state.o client.o -o client.exe -lws2_32
