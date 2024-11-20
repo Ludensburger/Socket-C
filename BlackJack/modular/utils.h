5b907c5fb5633f63053e5f09fa24892699af336b
Ludensburger <mendozaryu08@gmail.com>
2024-11-21 00:29:12 +0800

    Add function prototypes for player handling and debugging utilities

--- BlackJack/modular/utils.h
+++ BlackJack/modular/utils.h
@@ -3,11 +3,15 @@
 
 #define BUFFER_SIZE 4096 // Increased buffer size
 
+#include "player.h" // Include the header file where Player is defined
+
 extern const char *DEALER_STRING;
 
 const char *card_to_string(int card);
 const char *getRandomColor();
 void printBanner();
 void error_exit(const char *message);
+const char *getColor(int choice);
+void print_debug_info(Player *dealer, Player players[], int player_count);
 
 #endif // UTILS_H
