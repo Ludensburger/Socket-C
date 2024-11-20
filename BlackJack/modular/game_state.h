211780f45ce962ab90743b13811a8cb56990af32
Ludensburger <mendozaryu08@gmail.com>
2024-11-21 00:28:41 +0800

    Refactor dealer turn function: add player array and count parameters for improved game logic

--- BlackJack/modular/game_state.h
+++ BlackJack/modular/game_state.h
@@ -7,7 +7,7 @@
 void deal_initial_cards(Player players[], int player_count, Player *dealer, Stack *cardStack);
 void send_game_state(Player players[], int player_count, Player *dealer);
 void prompt_player_action(Player players[], int player_count, Player *player, Player *dealer, Stack *cardStack);
-void dealer_turn(Player *dealer, Stack *cardStack);
+void dealer_turn(Player *dealer, Stack *cardStack, Player players[], int player_count);
 void determine_winners(Player players[], int player_count, Player *dealer);
 
 #endif // GAME_STATE_H
