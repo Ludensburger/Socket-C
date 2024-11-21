454d447c916ebdbfdcb791c6526d0670a6c06f72
Ludensburger <mendozaryu08@gmail.com>
2024-11-21 09:33:44 +0800

    Add build commands for easy and probabilistic server modes in build.sh

--- BlackJack/monolithic/build.sh
+++ BlackJack/monolithic/build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
 # Compile server.c with ws2_32 library
+# Default server
 gcc -o server server.c -lws2_32
+
+# Easy server
+# Default Name: server-easy
+gcc -o server-easy server-easy.c -lws2_32
+
+# Probabilistic server
+# Default Name: server-probabilistic
+gcc -o server-probabilistic server-probabilistic.c -lws2_32
+
+# Easy Mode
+gcc -o easy server-easy.c -lws2_32
+
+# Probabilistic Mode
+gcc -0 smart server-probabilistic.c -lws2_32
+
 
 # Compile client.c with ws2_32 library
 gcc -o client client.c -lws2_32
