88dec789c31b1df60ab758362d4ced46c0a09c56
Ludensburger <mendozaryu08@gmail.com>
2024-12-18 00:59:38 +0800

    Add build targets for start and join executables in build script

--- BlackJack/monolithic/Smart/build.sh
+++ BlackJack/monolithic/Smart/build.sh
@@ -4,13 +4,16 @@
 
 # Default Name: server-probabilistic
 gcc -o server-probabilistic server-probabilistic.c -lws2_32
+gcc -o start server-probabilistic.c -lws2_32
 
 # Remove existing client executable if it exists
 if [ -f client.exe ]; then
     rm client.exe
+    rm join.exe
 fi
 
 # Compile client.c with ws2_32 library
 gcc -o client client.c -lws2_32
+gcc -o join client.c -lws2_32
 
 echo "Build complete. Executables are smart-server and client."
